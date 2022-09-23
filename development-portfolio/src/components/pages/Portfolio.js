27a11ed3c87eb957256ae14d9f6e384c6869f292
Max McDonough <max.mcdonough@gmail.com>
2022-09-23 11:58:59 -0400

    Added Portfolio page layout

--- development-portfolio/src/components/pages/Portfolio.js
+++ development-portfolio/src/components/pages/Portfolio.js
@@ -2,6 +2,46 @@ import React from "react";
 
 export default function Portfolio() {
     return (
-        <h3>Portfolio Content</h3>
+        <div>
+            <h2>Portfolio Content</h2>
+            <div className="d-flex flex-row flex-wrap justify-content-center">
+                <div className="card text-black col-5 m-4">
+                    <img className="" src="https://via.placeholder.com/500x200"></img>
+                    <div className="card-img-overlay">
+                        <h5 className="card-title">Project Name</h5>
+                    </div>
+                </div>
+                <div className="card text-black col-5 m-4">
+                    <img className="" src="https://via.placeholder.com/500x200"></img>
+                    <div className="card-img-overlay">
+                        <h5 className="card-title">Project Name</h5>
+                    </div>
+                </div>
+                <div className="card text-black col-5 m-4">
+                    <img className="" src="https://via.placeholder.com/500x200"></img>
+                    <div className="card-img-overlay">
+                        <h5 className="card-title">Project Name</h5>
+                    </div>
+                </div>
+                <div className="card text-black col-5 m-4">
+                    <img className="" src="https://via.placeholder.com/500x200"></img>
+                    <div className="card-img-overlay">
+                        <h5 className="card-title">Project Name</h5>
+                    </div>
+                </div>
+                <div className="card text-black col-5 m-4">
+                    <img className="" src="https://via.placeholder.com/500x200"></img>
+                    <div className="card-img-overlay">
+                        <h5 className="card-title">Project Name</h5>
+                    </div>
+                </div>
+                <div className="card text-black col-5 m-4">
+                    <img className="" src="https://via.placeholder.com/500x200"></img>
+                    <div className="card-img-overlay">
+                        <h5 className="card-title">Project Name</h5>
+                    </div>
+                </div>
+            </div>
+        </div>   
     )
 };
