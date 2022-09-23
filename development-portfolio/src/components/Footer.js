d8db290718f693bf0ea5a2ebceddda4ffb39890d
Max McDonough <max.mcdonough@gmail.com>
2022-09-23 15:06:22 -0400

    Added react-icons and built footer

--- development-portfolio/src/components/Footer.js
+++ development-portfolio/src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
 
 export default function Footer() {
     return (
-        <footer className="d-flex justify-content-center">
-            <h4> This is the Footer</h4>
+        <footer>
+            <h1 className="d-flex justify-content-center">
+                <a className="m-2" href="https://github.com/MMMPhoto" target="_blank"><AiFillGithub /></a>
+                <a className="m-2" href="https://www.linkedin.com/in/maxmcdonough/" target="_blank"><AiFillLinkedin /></a>
+                <a className="m-2" href="https://twitter.com/maxmcdonough" target="_blank"><AiFillTwitterCircle /></a>
+            </h1>
         </footer>
     )
 };
