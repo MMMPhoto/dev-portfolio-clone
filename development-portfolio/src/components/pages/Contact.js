71b07ee54cc735b8e9f6fecd5d99644ca193ee41
Max McDonough <max.mcdonough@gmail.com>
2022-09-23 14:34:46 -0400

    Added emailJS for contact form

--- development-portfolio/src/components/pages/Contact.js
+++ development-portfolio/src/components/pages/Contact.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+    const form = useRef();
+
+    const sendEmail = (event) => {
+        event.preventDefault();
+
+        emailjs.sendForm('service_6912jgl', 'template_nxmathh', form.current, 'IIBjjcjGKvHQDurqZ')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    };
+
     return (
         <div>
             <h3>Contact</h3>
-            <form className="form-control border-0">
+            <form ref={form} onSubmit={sendEmail} className="form-control border-0">
                 <div className="form-group m-2">
                     <label for="name">Name</label>
-                    <input name="name" type="text" className="form-control col-8"></input>    
+                    <input name="from_name" type="text" className="form-control col-8" />    
                 </div>
                 <div className="form-group m-2">
                     <label for="email">Email</label>
-                    <input name="email" type="email" className="form-control col-8"></input>    
+                    <input name="reply_to" type="email" className="form-control col-8" />    
                 </div>
                 <div className="form-group m-2">
                     <label for="message">Message</label>
-                    <textarea name="" type="text" className="form-control col-8"></textarea>    
+                    <textarea name="message" type="text" className="form-control col-8"></textarea>    
                 </div>
                 <div className="form-group m-2">
-                    <button type="submit" className="btn border-secondary">Submit</button>
+                    <input type="submit" value="Send" />
                 </div>
             </form>
         </div>
