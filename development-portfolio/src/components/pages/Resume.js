f1b5c6c3ee9a20d1471d8b614c6d33c50c8f00f6
Max McDonough <max.mcdonough@gmail.com>
2022-09-23 12:34:39 -0400

    Resume section

--- development-portfolio/src/components/pages/Resume.js
+++ development-portfolio/src/components/pages/Resume.js
@@ -2,6 +2,30 @@ import React from "react";
 
 export default function Resume() {
     return (
-        <h3>Resume Content</h3>
+        <div>
+             <h2>Resume</h2>
+                <h5><a href="">Download my Resume</a></h5>
+             <h4>Proficiencies</h4>
+                <ul>
+                    <li>List item</li>
+                    <li>List item</li>
+                    <li>List item</li>
+                    <li>List item</li>
+                </ul>
+            <h4>Languages</h4>
+                <ul>
+                    <li>List item</li>
+                    <li>List item</li>
+                    <li>List item</li>
+                    <li>List item</li>
+                </ul>
+            <h4>Certifcations</h4>
+                <ul>
+                    <li>List item</li>
+                    <li>List item</li>
+                    <li>List item</li>
+                    <li>List item</li>
+                </ul>
+        </div>
     )
 };
