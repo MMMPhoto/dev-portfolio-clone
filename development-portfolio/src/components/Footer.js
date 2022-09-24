43ea01506d55ca2ba85bc1d49677b57b64b9b9c6
Max McDonough <max.mcdonough@gmail.com>
2022-09-24 17:13:06 -0400

    Added overlay styling to portfolio cards

--- development-portfolio/src/components/Footer.js
+++ development-portfolio/src/components/Footer.js
@@ -5,9 +5,9 @@ export default function Footer() {
     return (
         <footer>
             <h1 className="d-flex justify-content-center">
-                <a className="m-2" href="https://github.com/MMMPhoto" target="_blank"><AiFillGithub /></a>
-                <a className="m-2" href="https://www.linkedin.com/in/maxmcdonough/" target="_blank"><AiFillLinkedin /></a>
-                <a className="m-2" href="https://twitter.com/maxmcdonough" target="_blank"><AiFillTwitterCircle /></a>
+                <a className="m-2" href="https://github.com/MMMPhoto" target="_blank" rel="noreferrer"><AiFillGithub /></a>
+                <a className="m-2" href="https://www.linkedin.com/in/maxmcdonough/" target="_blank" rel="noreferrer"><AiFillLinkedin /></a>
+                <a className="m-2" href="https://twitter.com/maxmcdonough" target="_blank" rel="noreferrer"><AiFillTwitterCircle /></a>
             </h1>
         </footer>
     )
--- development-portfolio/src/components/pages/Portfolio.js
+++ development-portfolio/src/components/pages/Portfolio.js
@@ -2,6 +2,7 @@ import React from "react";
 import { AiFillGithub } from "react-icons/ai";
 
 import portfolioItems from "../../data/portfolioItems";
+import "../../styles/Portfolio.css";
 
 export default function Portfolio() {
 
@@ -11,11 +12,11 @@ export default function Portfolio() {
             <div className="d-flex flex-row flex-wrap justify-content-center">
                 {portfolioItems.slice(0).reverse().map((item) => (
                     <div key={item.id} className="card text-black col-5 m-4">
-                        <img className="" src={item.imgSrc}></img>
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">
-                                <a className="m-2" href={item.deploymentUrl} target="_blank">{item.name}</a>
-                                <a className="m-2" href={item.githubRepo} target="_blank"><AiFillGithub /></a>
+                        <img src={item.imgSrc} alt={item.name}></img>
+                        <div className="card-img-overlay overlay">
+                            <h5 className="card-title w-100 h-100">
+                                <a className="m-2" href={item.deploymentUrl} target="_blank" rel="noreferrer">{item.name}</a>
+                                <a className="m-2" href={item.githubRepo} target="_blank" rel="noreferrer"><AiFillGithub /></a>
                             </h5>
                         </div>
                     </div>
@@ -24,36 +25,3 @@ export default function Portfolio() {
         </div>   
     )
 };
-
-/*                 
-                <div className="card text-black col-5 m-4">
-                    <img className="" src="https://via.placeholder.com/500x200"></img>
-                    <div className="card-img-overlay">
-                        <h5 className="card-title">Project Name</h5>
-                    </div>
-                </div>
-                <div className="card text-black col-5 m-4">
-                    <img className="" src="https://via.placeholder.com/500x200"></img>
-                    <div className="card-img-overlay">
-                        <h5 className="card-title">Project Name</h5>
-                    </div>
-                </div>
-                <div className="card text-black col-5 m-4">
-                    <img className="" src="https://via.placeholder.com/500x200"></img>
-                    <div className="card-img-overlay">
-                        <h5 className="card-title">Project Name</h5>
-                    </div>
-                </div>
-                <div className="card text-black col-5 m-4">
-                    <img className="" src="https://via.placeholder.com/500x200"></img>
-                    <div className="card-img-overlay">
-                        <h5 className="card-title">Project Name</h5>
-                    </div>
-                </div>
-                <div className="card text-black col-5 m-4">
-                    <img className="" src="https://via.placeholder.com/500x200"></img>
-                    <div className="card-img-overlay">
-                        <h5 className="card-title">Project Name</h5>
-                    </div>
-                </div>
-            </div> */
