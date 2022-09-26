236d617baf084412f87dcfabc3c22353c3b0bdf2
Max McDonough <max.mcdonough@gmail.com>
2022-09-25 20:29:15 -0400

    Added header styling file, mmm Logo

--- development-portfolio/src/components/Header.js
+++ development-portfolio/src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import mmmLogo from "../images/mmm-web-logo.svg";
 
 export default function Header({pageList, currentPage, handlePage}) {
 
     return (
         <header className="col-6 p-3">
             <h1>Max McDonough</h1>
+            <img src={mmmLogo} alt="mmm site logo" />
         </header>
     );
 };
