06586c8764ca123c100d8aada64d33bb7403f1a4
Max McDonough <max.mcdonough@gmail.com>
2022-09-27 12:31:28 -0400

    Set up portfolio image file

--- development-portfolio/src/components/pages/Portfolio.js
+++ development-portfolio/src/components/pages/Portfolio.js
@@ -12,7 +12,9 @@ export default function Portfolio() {
             <div className="d-flex flex-row flex-wrap justify-content-center">
                 {portfolioItems.slice(0).reverse().map((item) => (
                     <div key={item.id} className="card text-black col-5 m-4">
-                        <img src={item.imgSrc} alt={item.name}></img>
+                        <div className="d-flex justify-content-center align-items-start overflow-hidden">
+                            <img className="flex-shrink-0 portfolio-img" src={item.imgSrc} alt={item.name}></img>
+                        </div>
                         <div className="card-img-overlay overlay">
                             <h5 className="card-title w-100 h-20">
                                 <a className="m-2 portfolio-link" href={item.deploymentUrl} target="_blank" rel="noreferrer">{item.name}</a>
--- development-portfolio/src/data/portfolioItems.js
+++ development-portfolio/src/data/portfolioItems.js
@@ -1,14 +1,32 @@
+import adoptMeScreenshot from "../images/Adopt-Me-Screenshot.png";
+
 const portfolioItems = [
     {
         id: 1,
+        name: "Weather App",
+        description: "Web APIs, Javascript, CSS, HTML",
+        deploymentUrl: "https://mmmphoto.github.io/Weather-App/",
+        githubRepo: "https://github.com/MMMPhoto/Weather-App",
+        imgSrc: "https://raw.githubusercontent.com/MMMPhoto/Weather-App/main/assets/images/weather-dashboard-screenshot.png"
+    },
+    {
+        id: 2,
+        name: "Event Finder",
+        description: "Web APIs, Javascript, CSS, HTML",
+        deploymentUrl: "https://mmmphoto.github.io/Event-Finder/",
+        githubRepo: "https://github.com/MMMPhoto/Event-Finder",
+        imgSrc: "https://user-images.githubusercontent.com/70594281/174419344-26644f83-8f0c-4395-9b49-65ef8950d945.png"
+    },
+    {
+        id: 3,
         name: "Adopt Me",
         description: "MVC, Node, Express, Sequelize, SQL",
         deploymentUrl: "https://adoptme5.herokuapp.com/",
         githubRepo: "https://github.com/JasmineDaniels/AdoptMe",
-        imgSrc: "https://via.placeholder.com/500x200"
+        imgSrc: `${adoptMeScreenshot}`
     },
     {
-        id: 2,
+        id: 4,
         name: "Tech Blog",
         description: "MVC, Node, Express, Sequelize, SQL",
         deploymentUrl: "https://max-tech-blog.herokuapp.com/",
@@ -16,7 +34,7 @@ const portfolioItems = [
         imgSrc: "https://raw.githubusercontent.com/MMMPhoto/Tech-Blog/main/assets/images/tech-blog-screenshot-1.png"
     },
     {
-        id: 3,
+        id: 5,
         name: "Social Media API",
         description: "Node, Express, Mongoose, Mongo (server-side only)",
         deploymentUrl: "https://github.com/MMMPhoto/Social-Network-API",
@@ -24,7 +42,7 @@ const portfolioItems = [
         imgSrc: "https://raw.githubusercontent.com/MMMPhoto/Social-Network-API/main/assets/social-network-api-screenshot-1.png"
     },
     {
-        id: 4,
+        id: 6,
         name: "Development Portfolio",
         description: "React",
         deploymentUrl: "https://mmmphoto.github.io/Development-Portfolio/",
