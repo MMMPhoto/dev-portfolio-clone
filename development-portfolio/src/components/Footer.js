afc8cf123b4f4a44ded51affcd8cd2980d11dead
Max McDonough <max.mcdonough@gmail.com>
2022-09-27 09:03:36 -0400

    Styling for footer, rearranging css files

--- development-portfolio/src/components/Footer.js
+++ development-portfolio/src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
+import "../styles/Footer.css";
 
 export default function Footer() {
     return (
         <footer>
-            <h1 className="d-flex justify-content-center">
+            <h1 className="d-flex justify-content-center" id="footer-icon">
                 <a className="m-2" href="https://github.com/MMMPhoto" target="_blank" rel="noreferrer"><AiFillGithub /></a>
                 <a className="m-2" href="https://www.linkedin.com/in/maxmcdonough/" target="_blank" rel="noreferrer"><AiFillLinkedin /></a>
                 <a className="m-2" href="https://twitter.com/maxmcdonough" target="_blank" rel="noreferrer"><AiFillTwitterCircle /></a>
