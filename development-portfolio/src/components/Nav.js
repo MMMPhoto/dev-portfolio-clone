7ab5d6ba5e1a6dfa5ae1c1c4306cdeef3661943c
Max McDonough <max.mcdonough@gmail.com>
2022-09-28 14:48:39 -0400

    Addedempty form validation

--- development-portfolio/src/components/Nav.js
+++ development-portfolio/src/components/Nav.js
@@ -6,7 +6,7 @@ export default function Nav({pages, currentPage, handlePage}) {
         <nav className="col-6 p-3 align-self-end">
             <ul className="nav nav-pills nav-fill flex-row justify-content-end navbar-nav">
                 {pages.map((page) => (
-                    <li key={page.id} class="nav-item m-2">
+                    <li key={page.id} className="nav-item m-2">
                         <a href={page.url} onClick={() => handlePage(page.name)} className={page.name === currentPage ? "nav-link active activeBtn" : "nav-link"}>{page.name}</a>
                     </li>
                 ))}
--- development-portfolio/src/components/pages/Contact.js
+++ development-portfolio/src/components/pages/Contact.js
@@ -1,18 +1,28 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 export default function Contact() {
     const form = useRef();
+    const [formName, setName] = useState('');
+    const [formEmail, setEmail] = useState('');
+    const [formMessage, setMessage] = useState('');
+    const [warningMessage, setWarningMessage] = useState('');
 
     const sendEmail = (event) => {
         event.preventDefault();
 
+        if (formName.trim().length === 0 || formEmail.trim().length === 0 || formMessage.trim().length === 0) {
+            setWarningMessage('All fields must be completed!');
+            return;
+        } else setWarningMessage('');
+
         emailjs.sendForm('service_6912jgl', 'template_nxmathh', form.current, 'IIBjjcjGKvHQDurqZ')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+            .then((result) => {
+                console.log(result.text);
+                setWarningMessage('Email sent!');
+            }, (error) => {
+                console.log(error.text);
+            });
     };
 
     return (
@@ -20,19 +30,20 @@ export default function Contact() {
             <h3>Contact</h3>
             <form ref={form} onSubmit={sendEmail} className="form-control border-0">
                 <div className="form-group m-2">
-                    <label for="name">Name</label>
-                    <input name="from_name" type="text" className="form-control col-8" />    
+                    <label htmlFor="from_name">Name</label>
+                    <input name="from_name" type="text" value={formName} onChange={(e) => setName(e.target.value)} className="form-control col-8" />    
                 </div>
                 <div className="form-group m-2">
-                    <label for="email">Email</label>
-                    <input name="reply_to" type="email" className="form-control col-8" />    
+                    <label htmlFor="reply_to">Email</label>
+                    <input name="reply_to" type="email" value={formEmail} onChange={(e) => setEmail(e.target.value)} className="form-control col-8" />    
                 </div>
                 <div className="form-group m-2">
-                    <label for="message">Message</label>
-                    <textarea name="message" type="text" className="form-control col-8"></textarea>    
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" type="text" value={formMessage} onChange={(e) => setMessage(e.target.value)} className="form-control col-8"></textarea>    
                 </div>
-                <div className="form-group m-2">
+                <div className="form-group m-2 d-flex flex-row">
                     <input type="submit" value="Send" />
+                    <p className="ms-5">{warningMessage}</p>                 
                 </div>
             </form>
         </div>
