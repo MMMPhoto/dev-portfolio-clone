7645c7bcbb5bedad5cd1899709603df87c7ca15b
Max McDonough <max.mcdonough@gmail.com>
2022-11-01 12:08:37 -0400

    Added Stack overflow icon and link to footer

--- development-portfolio/src/components/Footer.js
+++ development-portfolio/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { ImStackoverflow } from "react-icons/im";
+
 import "../styles/Footer.css";
 
 export default function Footer() {
@@ -7,7 +9,7 @@ export default function Footer() {
         <footer className="d-flex justify-content-center">
             <a className="m-2 footer-icon" href="https://github.com/MMMPhoto" target="_blank" rel="noreferrer"><AiFillGithub /></a>
             <a className="m-2 footer-icon" href="https://www.linkedin.com/in/maxmcdonough/" target="_blank" rel="noreferrer"><AiFillLinkedin /></a>
-            <a className="m-2 footer-icon" href="https://twitter.com/maxmcdonough" target="_blank" rel="noreferrer"><AiFillTwitterCircle /></a>
+            <a className="m-2 footer-icon" href="https://stackoverflow.com/users/18729114/waxmell" target="_blank" rel="noreferrer"><ImStackoverflow /></a>
         </footer>
     )
 };
