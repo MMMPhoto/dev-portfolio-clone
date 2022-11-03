68c5d10e5f4a3ff9ac23d19bebc716f6f561766c
Max McDonough <max.mcdonough@gmail.com>
2022-11-03 14:56:59 -0400

    Added react-router-dom to home

--- development-portfolio/src/components/Container.js
+++ development-portfolio/src/components/Container.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
 import Header from "./Header";
 import Nav from "./Nav";
@@ -11,31 +12,39 @@ import pageList from "../data/pageList";
 export default function Container() {
     const [currentPage, setCurrentPage] = useState('About');
 
-    const renderPage = () => {
-        switch (currentPage) {
-            case "About":
-                return <About />;
-            case "Portfolio":
-                return <Portfolio />;
-            case "Contact":
-                return <Contact />;
-            default:
-                return <Resume />;
-        };
-    };
+    // const renderPage = () => {
+    //     switch (currentPage) {
+    //         case "About":
+    //             return <About />;
+    //         case "Portfolio":
+    //             return <Portfolio />;
+    //         case "Contact":
+    //             return <Contact />;
+    //         default:
+    //             return <Resume />;
+    //     };
+    // };
 
     const handlePage = (page) => setCurrentPage(page);
 
     return (
         <div>
-            <div className="d-flex flex-column flex-lg-row justify-content-between">
-                <Header pageList={pageList} currentPage={currentPage} handlePage={handlePage} />
-                <Nav pages={pageList} currentPage={currentPage} handlePage={handlePage}/>
-            </div>
-            <main className="p-3 pt-0 pt-lg-3 col-12 w-100">
-                {renderPage()}
-            </main>
-            <Footer />
+            <Router>
+                <div className="d-flex flex-column flex-lg-row justify-content-between">
+                    <Header pageList={pageList} currentPage={currentPage} handlePage={handlePage} />
+                    <Nav pages={pageList} currentPage={currentPage} handlePage={handlePage}/>
+                </div>
+                <main className="p-3 pt-0 pt-lg-3 col-12 w-100">
+                    <Routes>
+                        <Route exact path="/" element={<About />}/>
+                        <Route exact path="/porfolio" element={<Portfolio />}/>
+                        <Route exact path="/contact" element={<Contact />}/>
+                        <Route exact path="/resume" element={<Resume />}/>
+                    </Routes>
+                    {/* {renderPage()} */}
+                </main>
+                <Footer />
+            </Router>
         </div>
     )
 };
