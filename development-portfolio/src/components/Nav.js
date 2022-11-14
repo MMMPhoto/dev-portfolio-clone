ed277040e522a211ca97288bf21d0e5c5951b91f
Max McDonough <max.mcdonough@gmail.com>
2022-11-14 17:33:48 -0500

    Fixed nav bar color issue

--- development-portfolio/src/components/Nav.js
+++ development-portfolio/src/components/Nav.js
@@ -5,15 +5,15 @@ import { NavLink } from "react-router-dom";
 export default function Nav({pages, currentPage, handlePage}) { 
     return (
         <nav className="col-12 col-lg-6 p-3 align-self-center align-self-lg-end">
-            <ul className="nav nav-pills nav-fill flex-row justify-content-end navbar-nav">              
+            <div className="nav nav-pills nav-fill flex-row justify-content-end navbar-nav">              
                 {pages.map((page) => (
-                    <li key={page.id} className="nav-item col-4 col-lg-1 ms-5 me-5 ms-lg-4 me-lg-4">
-                        <NavLink className={page.name === currentPage ? "btn customActive" : "btn btn-light"} to={`/Development-Portfolio/${page.link}`}>
+                    <div key={page.id} className="nav-item col-4 col-lg-1 ms-5 me-5 ms-lg-4 me-lg-4">
+                        <NavLink className="nav-item nav-link" activeClassName="nav-item nav-link nav-link-active" exact to={`/Development-Portfolio/${page.link}`}>
                             {page.name}
                         </NavLink>
-                    </li>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </nav>
     );
 };
