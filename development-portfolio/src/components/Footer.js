77122763a689ede9dd28db164e4de894880ebfba
Max McDonough <max.mcdonough@gmail.com>
2022-11-15 13:29:28 -0500

    Fixed footer issue

--- development-portfolio/src/components/Footer.js
+++ development-portfolio/src/components/Footer.js
@@ -1,18 +1,14 @@
 import React from "react";
-import AbsoluteWrapper from "./AbsoluteWrapper";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { ImStackoverflow } from "react-icons/im";
-
 import "../styles/Footer.css";
 
 export default function Footer() {
     return (
-        <AbsoluteWrapper>
-                <footer className="d-flex justify-content-center">
-                    <a className="m-2 footer-icon" href="https://github.com/MMMPhoto" target="_blank" rel="noreferrer"><AiFillGithub /></a>
-                    <a className="m-2 footer-icon" href="https://www.linkedin.com/in/maxmcdonough/" target="_blank" rel="noreferrer"><AiFillLinkedin /></a>
-                    <a className="m-2 footer-icon" href="https://stackoverflow.com/users/18729114/waxmell" target="_blank" rel="noreferrer"><ImStackoverflow /></a>
-                </footer>
-        </AbsoluteWrapper>
+            <footer className="d-flex justify-content-center">
+                <a className="m-2 footer-icon" href="https://github.com/MMMPhoto" target="_blank" rel="noreferrer"><AiFillGithub /></a>
+                <a className="m-2 footer-icon" href="https://www.linkedin.com/in/maxmcdonough/" target="_blank" rel="noreferrer"><AiFillLinkedin /></a>
+                <a className="m-2 footer-icon" href="https://stackoverflow.com/users/18729114/waxmell" target="_blank" rel="noreferrer"><ImStackoverflow /></a>
+            </footer>
     );
 };
