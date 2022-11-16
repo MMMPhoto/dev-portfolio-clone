198464c9737410b03321172437a347f43c2c850d
Max McDonough <max.mcdonough@gmail.com>
2022-11-16 11:17:16 -0500

    Added config duration and easing, fixed header jiggle

--- development-portfolio/src/components/Container.js
+++ development-portfolio/src/components/Container.js
@@ -1,6 +1,6 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import React from "react";
-import { useTransition, animated } from "react-spring";
+import { useTransition, animated, config, easings } from "react-spring";
 import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
@@ -14,28 +14,29 @@ export default function Container() {
 
     let location = useLocation();
 
-    const transitions = useTransition(location, {
-        from: { position: "absolute", opacity: 0, transform: "translate3d(100%, 0, 0)" },
+    const slideLeftTransition = useTransition(location, {
+        from: { position: "absolute", opacity: 0, transform: "translate3d(100%, 0, 0)"},
         enter: { position: "relative", opacity: 1, transform: "translate3d(0%, 0, 0)" },
-        leave: { position: "absolute", opacity: 0, transform: "translate3d(-25%, 0, 0)"}
+        leave: { position: "absolute", opacity: 0, transform: "translate3d(-60%, 0, 0)"},
+        config: { duration: 600, easing: easings.easeInOutCubic },
     });
 
     return (
         <div>
-            <div className="d-flex flex-column flex-lg-row justify-content-between">
+            <div style={{ maxWidth: "99vw" }} className="d-flex flex-column flex-lg-row justify-content-between">
                 <Header />
                 <Nav pages={pageList} location={location} />
             </div>
-            {transitions((props, item) => (
-                <animated.div style={props}>
-                    <Routes location={item}>
-                        <Route path="/Development-Portfolio" element={<About />} />
-                        <Route path="/Development-Portfolio/work" element={<Portfolio />} />
-                        <Route path="/Development-Portfolio/contact" element={<Contact />} />
-                        <Route path="/Development-Portfolio/resume" element={<Resume />} />
-                    </Routes>
-                </animated.div>
-            ))}
+                {slideLeftTransition((props, item) => (
+                    <animated.div style={props} config={config} >
+                        <Routes location={item}>
+                            <Route path="/Development-Portfolio" element={<About />} />
+                            <Route path="/Development-Portfolio/work" element={<Portfolio />} />
+                            <Route path="/Development-Portfolio/contact" element={<Contact />} />
+                            <Route path="/Development-Portfolio/resume" element={<Resume />} />
+                        </Routes>
+                    </animated.div>
+                ))}
             <Footer />
         </div>
     )
